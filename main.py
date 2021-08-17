607237d4d5cd71b98c692b18f7f637cf751c5ead
subs001 <subramanian.v2k@gmail.com>
2021-08-17 15:51:25 +0530

    added subscribe function, object created for new reminders

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import datetime
 from datetime import date
+import random
 
 today = date.today()
 today = today.strftime("%d/%m/%Y")
@@ -12,6 +13,20 @@ load_dotenv()
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='$')
+
+reminderObject = dict()
+
+# function to create object to be stored in the databse
+def createObject(username, dateRequired, title):
+    objectID = random.randint(1000, 10000)
+    reminderObject[objectID] = {
+        "user": {
+            username: "Incomplete"
+        },
+        "date": dateRequired,
+        "title": title
+    }
+    return objectID
 
 # function to check if date entered is in the correct format
 def checkDate(input):
@@ -25,6 +40,7 @@ def checkDate(input):
 # async def on_ready():
 #     print('We have logged in as {0.user}'.format(client))
 
+# command to set reminders
 @bot.command(name='remind')
 async def getDate(ctx):
 
@@ -32,6 +48,7 @@ async def getDate(ctx):
     def check(msg):
         return msg.author == ctx.author and msg.channel == ctx.channel
         
+    # ensuring bot doesn't reply to its own messages    
     if ctx.author == client.user:
         return
     
@@ -39,19 +56,46 @@ async def getDate(ctx):
     await ctx.send('Enter the date in the d/m/y format along with the title of the reminder')
     message = await bot.wait_for("message", check=check)
 
+    # split message into date and title of the reminder
     content = message.content.split(' ',1)
     if(len(content) == 2):
         if(checkDate(content[0])):
             await message.channel.send('Do you want to set a reminder at: ' + content[0] + '?(yes/no)')
             confirmation = await bot.wait_for("message", check=check)
             if(confirmation.content.lower() == 'yes'):
-                await message.channel.send("Reminder Set!")
+                # sending username in the form of NAME#ID, similar to how discord identifies users
+                ID = createObject(ctx.author.name + '#' + ctx.author.discriminator, content[0], content[1])
+                await message.channel.send("Reminder Set! ID: " + str(ID))
             else:
                 await message.channel.send("Reminder Not Set!")
                 return
         else:
             await message.channel.send('Invalid Syntax')
             
+
+# command to subsribe to an existing reminder
+@bot.command(name='sub')
+async def subscribe(ctx):
+
+    # ensuring bot doesn't reply to its own messages    
+    if ctx.author == client.user:
+        return
+
+    # function to check if the confirmation message was sent by the same user
+    def check(msg):
+        return msg.author == ctx.author and msg.channel == ctx.channel
+
+    await ctx.channel.send('Enter the ID of the reminder')
+    message = await bot.wait_for("message", check=check)
+    ID = int(message.content)
+    # check if the id exists
+    if ID in reminderObject:
+        reminderObject[ID]["user"][ctx.author.name + '#' + ctx.author.discriminator] = "Incomplete"
+        print(reminderObject)
+        await ctx.channel.send('Subscribed!')
+    else:
+        await ctx.channel.send('ID not found!')
+        
 
 TOKEN = os.environ['TOKEN']
 bot.run(TOKEN)
